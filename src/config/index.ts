78135fd3097034ecc7bd3af6e6a803a8ab63119d
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-12-06 03:46:44 +0600

    set the .env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,16 @@
 import dotenv from "dotenv";
 import path from "path";
-
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
+dotenv.config({ path: path.join(process.cwd(), envFile) });
 
 const config = {
   connection_str: process.env.CONNECTION_STR,
   port: process.env.PORT,
   jwtSecret: process.env.JWT_SECRET,
+  nodeEnv: process.env.NODE_ENV,
 };
 
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import app from './app';
-import { initializeDatabase } from './config/database';
-
-const PORT = process.env.PORT || 5000;
+import app from "./app";
+import config from "./config";
+import { initializeDatabase } from "./config/database";
 
 const startServer = async () => {
   try {
     // Initialize database
     await initializeDatabase();
 
-    app.listen(PORT, () => {
-      console.log(`🚀 Server is running on port ${PORT}`);
-      console.log(`📍 API Base URL: http://localhost:${PORT}/api/v1`);
+    app.listen(config.port, () => {
+      console.log(`🚀 Server is running on port ${config.port}`);
+      console.log(`📍 API Base URL: ${config.nodeEnv}:${config.port}/api/v1`);
     });
   } catch (error) {
-    console.error('❌ Failed to start server:', error);
+    console.error("❌ Failed to start server:", error);
     process.exit(1);
   }
 };
